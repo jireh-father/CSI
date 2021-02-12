21fb1839ce538ad99535405e16c8a88133d17094
seoilgun <seoilgun@gmail.com>
2021-02-12 16:35:10 +0900

    add skin dataset

--- evals/ood_pre.py
+++ evals/ood_pre.py
@@ -180,7 +180,7 @@ def _get_features(P, model, loader, interp=False, imagenet=False, simclr_aug=Non
             x_interp = (x + last) / 2 if i > 0 else x  # omit the first batch, assume batch sizes are equal
             last = x  # save the last batch
             x = x_interp  # use interp as current batch
-        print(0, _.shape)
+        print(0, _.shape, _)
         print(1, x)
         if imagenet is True:
             x = torch.cat(x[0], dim=0)  # augmented list of x
